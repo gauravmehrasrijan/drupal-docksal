5ea03dbbc8ac6fc4b3348bba8c1a4fd2cc8ba0c5
Gaurav Mehra <gaurav.mehra@srijan.net>
2020-04-26 10:22:47 +0530

    push master

--- docroot/themes/custom/occean/gulpfile.js
+++ docroot/themes/custom/occean/gulpfile.js
@@ -18,5 +18,5 @@ catch (e) {
 require('emulsify-gulp')(gulp, localConfig);
 
 //
-//  Trying to produce a git conflict here at master branch
+//  Trying to produce a git conflict here at (MASTER) branch
 //
